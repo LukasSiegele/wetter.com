a0f194abed70ebf84b864192c943bff44d35ac4e
LukasSiegele <lukas.siegele@hfg-gmuend.de>
2015-06-08 11:41:39 +0200

    intro_8. Juni 2015

---
+++ prototyp_intro/scripts/script.js
@@ -0,0 +1,83 @@
+$(document).ready(function()	{
+	
+	// Hier steht unser Code
+
+
+$(".suche").click( function(){
+
+		$(this).toggleClass("active_suche");
+		$(".container_diagramm").toggleClass("active_dia");
+		$(".container_vohersage").toggleClass("active_vohersage");
+
+
+	});
+
+$(".suche").hover( function(){
+
+		$(".favo").toggleClass("hover_fav");
+
+
+
+	});
+
+
+$(".container_suche").click( function(){
+
+		$(this).toggleClass("active");
+		$(".favo").toggleClass("active_fav");
+
+
+
+	});
+
+$(".background").click( function(){
+
+		$(this).toggleClass("active_karte");
+
+		$(".container_suche").toggleClass("active_back");
+		$(".container_diagramm").toggleClass("active_back");
+		$(".container_vohersage").toggleClass("active_back");
+
+	});
+
+$(".container_vohersage").click( function(){
+
+		$(this).toggleClass("click_v");
+
+		$(".background").toggleClass("fadeout");
+
+
+
+	});
+
+$(".profilpic").click( function(){
+
+		
+
+		$(".profileinst").toggleClass("active_profil");
+
+
+
+	});
+
+$(window).load( function(){
+
+		setTimeout(function(){
+  		$(".tipp").addClass("showup");
+	}	, 2000);
+
+
+		setTimeout(function(){
+  		$(".tipp").removeClass("showup");
+	}	, 5000);
+
+
+
+});
+
+
+$(document).on( 'scroll', '.container_vohersage', function(){
+    alert("HIlfe");
+});
+
+});
